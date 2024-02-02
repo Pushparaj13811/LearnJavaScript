376e3057a550b6ad51d4dc13e9042d2577aeb707
Hompushparaj Mehta <hmehta588@rku.ac.in>
2024-02-02 13:05:51 +0000

    Go through with some exampel of datatypes

---
+++ basic/datatype.js
@@ -0,0 +1,17 @@
+"use strict"; // We use this to give command to engine to treat all the code in newer version.
+//alert("Hello"); // It will give error when we execute it in node.js. It is workable only on browsers.
+
+// Datatypes of the js 
+//number
+//bigint
+//string
+//boolean
+//null => We can consider it at standalone value too
+//undefined => It is also a datatype. We get value undefined when we just declare the variable without assigning any value to it.
+//symbol => It is use to check the uniqueness.
+//Object 
+
+//null is the representation of the empty value.
+
+//To check the datatype of any variable or data we can use `typeof` function
+console.log(typeof 5);
