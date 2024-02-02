a3a895c7a2085ad88294f6ad550aa0879f5c8d58
Hompushparaj Mehta <hmehta588@rku.ac.in>
2024-02-02 16:26:26 +0000

    Get insight about comparison and equality check

---
+++ basic/comparison.js
@@ -0,0 +1,9 @@
+//Always prefer comparing the value of same datatype 
+
+console.log(null > 0) // It will return false
+console.log(null == 0) // It also return false
+console.log(null >= 0) // It will return true because the comparison operation and equality check == work differently 
+// comparison convert null into number i.e. 0 so the output of thid will true while the == check doesnot do such conversion 
+// resulting the output as flase for 2
+
+// strict check i.e. === It will check the value and datatype both 
--- basic/operations.js
+++ basic/operations.js
@@ -37,3 +37,4 @@ let gameCounter = 100
 gameCounter++
 console.log(gameCounter)
 */
+
