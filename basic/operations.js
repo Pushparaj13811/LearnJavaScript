268b2973f06d87bf726deee8168bec42dbca6f2a
Hompushparaj Mehta <hmehta588@rku.ac.in>
2024-02-02 16:15:55 +0000

    Basic operations of datatype

---
+++ basic/operations.js
@@ -0,0 +1,39 @@
+// *************************** Operations **********************
+/*
+let value = 3
+let negValue = -value
+console.log(negValue)
+*/
+
+/*
+let str1 = "Hello"
+let str2 = " Pushparaj"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2) // It will convert into string 
+console.log(1 + "2") // It will convert into string
+console.log("1" + 2 + 2) // It will convert every value into string
+console.log(1 + 2 + "2") // It will add first 2 and then convert it into string
+// If string value is in first then it will convert all of them into string 
+
+*/
+
+console.log(+true) // It will convert true into 1
+console.log(+"") // It will convert empty string into 0
+
+/*
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+console.log(num1)
+
+Do not prefer to write this type of confusing code
+*/
+
+/*
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+*/
