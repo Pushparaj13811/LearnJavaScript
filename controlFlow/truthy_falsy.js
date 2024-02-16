3618520933c8d927cccd7df9fe847db8d15e1567
Hompushparaj Mehta <hmehta588@rku.ac.in>
2024-02-16 04:03:40 +0000

    Truthy, falsy value, nullish Coalescing Operator and ternary Operator

--- controlFlow/truthy_falsy.js
+++ controlFlow/truthy_falsy.js
@@ -37,3 +37,23 @@ const emptyObj = {}
 if (Object.keys(emptyObj).length === 0) {
     console.log("Object is empty")
 }
+
+// Nullish Coalescing Operator(??) : null undefined
+// This operator check the safety of null 
+let val1;
+
+// val1 = 5 ?? 10 
+// val1 = null ?? 10 
+// val1 = undefined ?? 15
+
+val1 = null ?? 10 ?? 20 // In this case it will assign the first value which is not null
+
+console.log(val1)
+
+// Ternary operator 
+
+// condition ? true : false
+
+const iceTeaPrice = 60
+
+iceTeaPrice >= 80 ? console.log("Price is not in budget") : console.log("Price is in budget")
