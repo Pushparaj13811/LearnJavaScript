c4841447561581ec4cfb5b359b8a2d77b25e2668
Hompushparaj Mehta <hmehta588@rku.ac.in>
2024-02-17 02:49:22 +0000

    while and do while loop

---
+++ Iterations/02_whileAndDowhile.js
@@ -0,0 +1,19 @@
+let index = 1
+while (index <= 10) {
+    //console.log(`Value of index is ${index}`)
+    index += 2
+}
+
+let superHeroes = ['batman', 'ironman', 'flash', 'superman']
+let arr = 0
+while (arr < superHeroes.length) {
+    //console.log(`The superheroes at ${arr} is ${superHeroes[arr]}`)
+    arr += 1
+}
+
+// do while loop always run loop for one time then only it check the condition 
+let score = 11
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while (score <= 10)
