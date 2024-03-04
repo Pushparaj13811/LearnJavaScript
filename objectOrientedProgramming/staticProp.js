7de72175c10d574b851a0ac4122364a0307f4faa
Pushparaj13811 <hmehta588@rku.ac.in>
2024-03-04 08:32:36 +0530

    Story of static keyword

---
+++ objectOrientedProgramming/staticProp.js
@@ -0,0 +1,27 @@
+class User {
+  constructor(username) {
+    this.username = username;
+  }
+
+  logMe() {
+    console.log(`Username: ${this.username}`);
+  }
+  static createId() {
+    return `123`;
+  }
+}
+
+const Pushparaj = new User("Pushparaj");
+// console.log(Pushparaj.createId()); // Outputs
+
+class Teacher extends User {
+  constructor(username, email) {
+    super(username);
+    this.email = email;
+  }
+}
+
+const Alpana = new Teacher("Alpana", "Alpana@google.com");
+Alpana.logMe();
+console.log(Alpana.createId();)
+
